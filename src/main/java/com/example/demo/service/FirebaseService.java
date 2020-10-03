a61afc987611915db10ca7c14d1110844a91da59
aamir_vahora_2617 <aamirvahora2601@gmail.com>
2020-10-04 03:11:07 +0530

    first commit

---
+++ src/main/java/com/example/demo/service/FirebaseService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirebaseService {
+	
+	public String getMessage() {
+		return "api called successfully";
+	}
+
+}
