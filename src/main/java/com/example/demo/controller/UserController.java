c7433f1416aaeaf95aad63219c12428465b215c8
aamir_vahora_2617 <aamirvahora2601@gmail.com>
2020-10-04 11:04:15 +0530

    third commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.FirebaseService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 @RestController
 public class UserController {
@@ -13,11 +20,18 @@ public class UserController {
 	FirebaseService firebaseService;
 	
 	
-	@GetMapping("/getUserDetails")
-	private String getUserDetails()
-	{
+	@GetMapping("/getSuccess")
+	public String getMessage() {
 		return firebaseService.getMessage();
 	}
+	
+	@GetMapping("/getId")
+	public String getId(@RequestParam ("id") int id) {
+		return Integer.toString(id);
+	}
+	
+	
+	
 	
 	
 
